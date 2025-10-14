9162f64986136f7eb0f0c98161ecb877020cd13e
lolwngud <118458655+lolwngud@users.noreply.github.com>
2025-10-14 23:31:20 +0900

    day1half

---
+++ Beginner/Day1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int solution(int, int);
+
+int main() {
+    /*int a = solution(10,5);
+    int b = solution(7,2); */
+
+    int a = solition(3,2);
+    int b = solution(100,2);
+
+    printf("%d, %d",a,b);
+
+    return 0;
+}
